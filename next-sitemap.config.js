92b70f1605d484ab4213d200b4f85573231d1104
liwawil <liwa.johnson@gmail.com>
2024-10-27 14:36:13 -0400

    add robot.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://www.liwatecsoftware.xyz',
-  generateRobotsTxt: false, // (optional)
+  generateRobotsTxt: true, // (optional)
   // ...other options
 }
